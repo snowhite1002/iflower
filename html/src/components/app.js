bd97cabfd0e1fd4c3a88f6161fb9e9167c5155d6
xue.bai_2 <xue.bai_2@quanshi.com>
2017-05-05 13:29:14 +0800

    菜单栏代码提交

---
+++ html/src/components/app.js
@@ -0,0 +1,25 @@
+/**
+ * Created by xue.bai_2 on 2017/5/4.
+ */
+import React from 'react';
+import Menu from './menu.js';
+import Content from './content.js';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+
+
+        return (
+            <div className="app-container">
+                <Menu />
+                <Content />
+            </div>
+
+        );
+    }
+}
+export default App;
